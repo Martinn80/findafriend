5007d83d53246c454f64fc5859f7f2cdecdbb98c
Martin Nguyen <ngumartin@gmail.com>
2019-12-21 22:34:17 -0800

    prep for heroku

---
+++ data/friends.js
@@ -0,0 +1,43 @@
+let friends = [{
+    "name": "Floyd Mayweather",
+    "photo": "https://news.bitcoin.com/wp-content/uploads/2018/10/Mayweather-300x300.jpg",
+    "scores": [5, 4, 2, 5, 1, 1, 2, 1, 3, 5]
+}, {
+    "name": "Kevin Hart",
+    "photo": "https://aalbc.com/author-photos/Kevin_Hart.jpg",
+    "scores": [3, 4, 2, 4, 4, 1, 1, 2, 3, 4]
+}, {
+    "name": "Dwayne Johnson",
+    "photo": "http://cdn04.cdn.justjared.com/wp-content/uploads/headlines/2018/04/dwayne-trivia.jpg",
+    "scores": [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+}, {
+    "name": "Sean Penn",
+    "photo": "https://vz.cnwimg.com/thumbc-300x300/wp-content/uploads/2009/09/GettyImages-144722694.jpg",
+    "scores": [5, 2, 4, 3, 3, 2, 1, 3, 3, 4]
+}, {
+    "name": "Napoleon Dynamite",
+    "photo": "http://destinationnewbedford.org/wp-content/uploads/Napolean-Large-300x300.jpg",
+    "scores": [4, 1, 5, 3, 4, 5, 2, 3, 4, 5]
+}, {
+    "name": "Scarlett Johansson",
+    "photo": "https://nyppagesix.files.wordpress.com/2013/08/scarlett_johansson_startraks-300x300.jpg?quality=90&strip=all&w=300",
+    "scores": [4, 1, 5, 3, 4, 5, 2, 3, 4, 5]
+}, {
+    "name": "Kate Hudson",
+    "photo": "https://marriedbiography.com/wp-content/uploads/2017/02/hbz-kate-hudson-dec-jan-2014-001-aijjxy-sq-xln-jpg1.jpg",
+    "scores": [3, 4, 2, 4, 4, 1, 1, 2, 3, 4]
+}, {
+    "name": "Drew Barrymore",
+    "photo": "https://www.alux.com/wp-content/uploads/2017/09/Drew-Barrymore-Net-Worth-300x300.jpg",
+    "scores": [5, 4, 2, 5, 1, 1, 2, 1, 3, 5]
+}, {
+    "name": "Beyonce",
+    "photo": "https://thehotzoneusa.com/wp-content/uploads/2013/12/beyonce-150-300x300.jpg",
+    "scores": [1, 5, 1, 3, 5, 1, 4, 3, 2, 1]
+}, {
+    "name": "Tamera Mowry",
+    "photo": "http://cdn02.cdn.justjared.com/wp-content/uploads/headlines/2018/11/tamera-mowry-housley-posts-touching-tribute-to-niece-alaina.jpg",
+    "scores": [1, 3, 3, 1, 2, 4, 4, 1, 3, 1]
+}];
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-let express = require("express");
-let bodyParser = require("body-parser");
-let path = require("path");
-let PORT = process.env.PORT || 8000;
-let app = express();
+const express = require("express");
+const app = express();
+// const bodyParser = require("body-parser");
+// const path = require("path");
+const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("./public"));
+
+const apiRoutes = require("./routing/apiRoutes");
+app.use("/api", apiRoutes);
+
+const htmlRoutes = require("./routing/htmlRoutes");
+app.use("/", htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`listening on http://localhost:${PORT}`);
+    console.log(`listening on port http://localhost:${PORT}`);
 });
